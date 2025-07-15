b8413f9def7192f6b8fee376568876e1d1b9caf8
geovgy <54918343+geovgy@users.noreply.github.com>
2025-07-15 11:33:06 -0400

    Update with nerite values

--- src/once.ts
+++ src/once.ts
@@ -1,25 +1,22 @@
-import { EthersLiquity } from "@liquity/lib-ethers";
 import fs from "fs";
 import path from "path";
 import util from "util";
 
-import v2LegacyDeployment from "../addresses/legacy.json";
-import v2RelaunchDeployment from "../addresses/relaunch.json";
-import v2SepoliaDeployment from "../addresses/sepolia.json";
+import v2ProdDeployment from "../addresses/prod.json";
 import { getProvider } from "./connection";
-import { fetchLQTYCirculatingSupply } from "./fetchLQTYCirculatingSupply";
-import { fetchLUSDCBBAMMStats } from "./fetchLUSDCBBAMMStats";
-import { fetchLUSDTotalSupply } from "./fetchLUSDTotalSupply";
+// import { fetchLQTYCirculatingSupply } from "./fetchLQTYCirculatingSupply";
+// import { fetchLUSDCBBAMMStats } from "./fetchLUSDCBBAMMStats";
+// import { fetchLUSDTotalSupply } from "./fetchLUSDTotalSupply";
 import { fetchPrices } from "./fetchPrices";
 import { fetchV2Stats } from "./v2/fetchV2Stats";
 
 import {
   DUNE_SPV2_AVERAGE_APY_URL_MAINNET,
   DUNE_SPV2_UPFRONT_FEE_URL_MAINNET,
-  LQTY_CIRCULATING_SUPPLY_FILE,
-  LUSD_CB_BAMM_STATS_FILE,
-  LUSD_TOTAL_SUPPLY_FILE,
-  OUTPUT_DIR_V1,
+  // LQTY_CIRCULATING_SUPPLY_FILE,
+  // LUSD_CB_BAMM_STATS_FILE,
+  // LUSD_TOTAL_SUPPLY_FILE,
+  // OUTPUT_DIR_V1,
   OUTPUT_DIR_V2
 } from "./constants";
 
@@ -29,15 +26,14 @@ const panic = <T>(message: string): T => {
 
 const alchemyApiKey = process.env.ALCHEMY_API_KEY || undefined; // filter out empty string
 const duneApiKey: string = process.env.DUNE_API_KEY || panic("missing DUNE_API_KEY");
-const transposeApiKey: string = process.env.TRANSPOSE_API_KEY || panic("missing TRANSPOSE_API_KEY");
+// const transposeApiKey: string = process.env.TRANSPOSE_API_KEY || panic("missing TRANSPOSE_API_KEY");
 const coinGeckoDemoApiKey: string = process.env.COINGECKO_DEMO_KEY || panic("missing COINGECKO_DEMO_KEY");
 
-const lqtyCirculatingSupplyFile = path.join(OUTPUT_DIR_V1, LQTY_CIRCULATING_SUPPLY_FILE);
-const lusdTotalSupplyFile = path.join(OUTPUT_DIR_V1, LUSD_TOTAL_SUPPLY_FILE);
-const lusdCBBAMMStatsFile = path.join(OUTPUT_DIR_V1, LUSD_CB_BAMM_STATS_FILE);
+// const lqtyCirculatingSupplyFile = path.join(OUTPUT_DIR_V1, LQTY_CIRCULATING_SUPPLY_FILE);
+// const lusdTotalSupplyFile = path.join(OUTPUT_DIR_V1, LUSD_TOTAL_SUPPLY_FILE);
+// const lusdCBBAMMStatsFile = path.join(OUTPUT_DIR_V1, LUSD_CB_BAMM_STATS_FILE);
 
-const mainnetProvider = getProvider("mainnet", { alchemyApiKey });
-const sepoliaProvider = getProvider("sepolia", { alchemyApiKey });
+const arbitrumProvider = getProvider("arbitrum", { alchemyApiKey });
 
 interface Tree extends Record<string, string | Tree> {}
 
@@ -55,78 +51,51 @@ const writeTree = (parentDir: string, tree: Tree) => {
   }
 };
 
-EthersLiquity.connect(mainnetProvider)
-  .then(async liquity => {
-    const [
-      lqtyCirculatingSupply,
-      lusdTotalSupply,
-      lusdCBBAMMStats,
-      v2LegacyStats,
-      v2RelaunchStats,
-      v2SepoliaStats,
-      prices
-    ] = await Promise.all([
-      fetchLQTYCirculatingSupply(liquity),
-      fetchLUSDTotalSupply(liquity),
-      fetchLUSDCBBAMMStats(transposeApiKey),
-      fetchV2Stats({
-        deployment: v2LegacyDeployment,
-        provider: mainnetProvider,
-        duneSpApyUrl: null,
-        duneSpUpfrontFeeUrl: null,
-        duneApiKey
-      }),
-      fetchV2Stats({
-        deployment: v2RelaunchDeployment,
-        provider: mainnetProvider,
-        duneSpApyUrl: DUNE_SPV2_AVERAGE_APY_URL_MAINNET,
-        duneSpUpfrontFeeUrl: DUNE_SPV2_UPFRONT_FEE_URL_MAINNET,
-        duneApiKey
-      }),
-      fetchV2Stats({
-        deployment: v2SepoliaDeployment,
-        provider: sepoliaProvider,
-        duneSpApyUrl: null,
-        duneSpUpfrontFeeUrl: null,
-        duneApiKey
-      }),
-      fetchPrices({ coinGeckoDemoApiKey })
-    ]);
+async function main() {
+  const [
+    // lqtyCirculatingSupply,
+    // lusdTotalSupply,
+    // lusdCBBAMMStats,
+    v2ProdStats,
+    prices
+  ] = await Promise.all([
+    // fetchLQTYCirculatingSupply(liquity),
+    // fetchLUSDTotalSupply(liquity),
+    // fetchLUSDCBBAMMStats(transposeApiKey),
+    fetchV2Stats({
+      deployment: v2ProdDeployment,
+      provider: arbitrumProvider,
+      duneSpApyUrl: DUNE_SPV2_AVERAGE_APY_URL_MAINNET,
+      duneSpUpfrontFeeUrl: DUNE_SPV2_UPFRONT_FEE_URL_MAINNET,
+      duneApiKey
+    }),
+    fetchPrices({ coinGeckoDemoApiKey })
+  ]);
 
-    const v2Stats = {
-      ...v2RelaunchStats,
-      legacy: v2LegacyStats,
-      prices,
-      testnet: {
-        sepolia: v2SepoliaStats
-      }
-    };
+  const v2Stats = {
+    ...v2ProdStats,
+    prices
+  };
 
-    fs.mkdirSync(OUTPUT_DIR_V1, { recursive: true });
-    fs.writeFileSync(lqtyCirculatingSupplyFile, `${lqtyCirculatingSupply}`);
-    fs.writeFileSync(lusdTotalSupplyFile, `${lusdTotalSupply}`);
-    fs.writeFileSync(lusdCBBAMMStatsFile, JSON.stringify(lusdCBBAMMStats));
+  // fs.mkdirSync(OUTPUT_DIR_V1, { recursive: true });
+  // fs.writeFileSync(lqtyCirculatingSupplyFile, `${lqtyCirculatingSupply}`);
+  // fs.writeFileSync(lusdTotalSupplyFile, `${lusdTotalSupply}`);
+  // fs.writeFileSync(lusdCBBAMMStatsFile, JSON.stringify(lusdCBBAMMStats));
 
-    writeTree(OUTPUT_DIR_V2, v2Stats);
-    fs.writeFileSync(
-      path.join(OUTPUT_DIR_V2, "mainnet.json"),
-      JSON.stringify({ ...v2LegacyStats, prices }, null, 2)
-    );
-    fs.writeFileSync(
-      path.join(OUTPUT_DIR_V2, "ethereum.json"),
-      JSON.stringify({ ...v2RelaunchStats, prices }, null, 2)
-    );
-    fs.writeFileSync(
-      path.join(OUTPUT_DIR_V2, "testnet", "sepolia.json"),
-      JSON.stringify({ ...v2SepoliaStats, prices }, null, 2)
-    );
+  writeTree(OUTPUT_DIR_V2, v2Stats);
+  fs.writeFileSync(
+    path.join(OUTPUT_DIR_V2, "arbitrum.json"),
+    JSON.stringify({ ...v2ProdStats, prices }, null, 2)
+  );
 
-    console.log(`LQTY circulating supply: ${lqtyCirculatingSupply}`);
-    console.log(`LUSD total supply: ${lusdTotalSupply}`);
-    console.log("LUSD CB BAMM stats:", lusdCBBAMMStats);
-    console.log();
-    console.log("v2 stats:", util.inspect(v2Stats, { colors: true, depth: null }));
-  })
+  // console.log(`LQTY circulating supply: ${lqtyCirculatingSupply}`);
+  // console.log(`LUSD total supply: ${lusdTotalSupply}`);
+  // console.log("LUSD CB BAMM stats:", lusdCBBAMMStats);
+  console.log();
+  console.log("v2 stats:", util.inspect(v2Stats, { colors: true, depth: null }));
+}
+
+main()
   .catch(error => {
     console.error(error);
     process.exit(1);
